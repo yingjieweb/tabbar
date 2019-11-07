4cc83e78d7334219a03a36e38ed401a1c021ff52
yingjieweb <1184061078@qq.com>
2019-11-07 20:17:32 +0800

    'slot'

---
+++ src/components/common/TabBarItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="tab-bar-item" @click="click">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{active:isActive}"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    data(){
+      return{
+        isActive:false
+      }
+    },
+    methods:{
+      click(){
+        console.log("组件被点击了");
+        return this.isActive = !this.isActive
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #tab-bar-item{
+    flex: 1;  /*弹性布局的均匀分布*/
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+ #tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+  .active{
+    color: red;
+  }
+</style>
---
+++ src/components/content/MainTabBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <tab-bar>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/home.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/home_active.svg" alt="">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/category.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/category_active.svg" alt="">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/shopcart.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/shopcart_active.svg" alt="">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item>
+      <img slot="item-icon" src="~assets/img/profile.svg" alt="">
+      <img slot="item-icon-active" src="~assets/img/profile_active.svg" alt="">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+  import TabBar from 'components/common/TabBar'
+  import TabBarItem from 'components/common/TabBarItem'
+  export default {
+    name: "MainTabBar",
+    components:{
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
